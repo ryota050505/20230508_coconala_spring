592bb00b84888fae1156acf64857fa98011cd3f7
ryota <05ryouta05@gmail.com>
2023-05-08 03:24:18 +0900

    不要なimplementsを削除

--- src/main/java/jp/co/sss/sys/model/LoginUser.java
+++ src/main/java/jp/co/sss/sys/model/LoginUser.java
@@ -2,14 +2,9 @@ package jp.co.sss.sys.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import org.springframework.context.annotation.Scope;
-import org.springframework.context.annotation.ScopedProxyMode;
-import org.springframework.stereotype.Component;
-
-import java.io.Serializable;
 
 @Data
 @AllArgsConstructor
-public class LoginUser implements Serializable {
+public class LoginUser {
      private String username;
 }
